1c6728dcb14f263cddf900a015812960d0b98ce4
Harun <harun.ozdemir@arneca.com>
2019-12-19 11:26:56 +0300

    swift version changed(swift 4.2)

--- colorjumping/GameScene.swift
+++ colorjumping/GameScene.swift
@@ -624,9 +624,10 @@ class GameScene: SKScene {
     }
     
     func randomNumber<T : SignedInteger>(inRange range: ClosedRange<T> = 1...6) -> T {
-        let length = (range.upperBound - range.lowerBound + 1).toIntMax()
-        let value = arc4random().toIntMax() % length + range.lowerBound.toIntMax()
-        return T(value)
+      let length = (range.upperBound - range.lowerBound + 1)
+      
+      let value = arc4random() % UInt32(length) + UInt32(range.lowerBound)
+      return T(value)
     }
     
 }
